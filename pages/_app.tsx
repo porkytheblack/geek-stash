5a3bcb7f63b65b9813540de527043caa1fed69f9
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2023-01-22 20:05:36 +0300

    chore: added title and description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import Layout from '../layout'
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs"
 import { Session, SessionContextProvider } from "@supabase/auth-helpers-react"
 import { useState } from 'react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps<IPageProps & {
   initialSession: Session
@@ -35,6 +36,12 @@ export default function App({ Component, pageProps }: AppProps<IPageProps & {
           <Provider
             store={store}
           >
+            <Head>
+              <title>Geek Stash</title>
+              <meta name="description" content="Build a custom api, for your favourite tvshow.Or use, what others have built" />
+              <link rel="icon" href="/favicon.ico" />
+              
+            </Head>
             <Layout pageProps={pageProps} >
               <Component {...pageProps} />
             </Layout>
